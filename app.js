13319c9972d8c4aa9997e4ec1db0076b580502e2
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-26 17:00:37 +0530

    file added

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-let smallImage = document.getElementsByClassName("oldImg");
-for(let i=0; i<smallImage.length; i++) {
-    smallImage[i].src = "assets/spiderman_img.png";
-}
+// let smallImage = document.getElementsByClassName("oldImg");
+// for(let i=0; i<smallImage.length; i++) {
+//     smallImage[i].src = "assets/spiderman_img.png";
+// }
+
+console.dir(document.querySelector("p"));
+
+console.dir(document.querySelectorAll("div a"));
